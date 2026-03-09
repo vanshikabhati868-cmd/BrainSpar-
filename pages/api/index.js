7b8b2e706b1b5774754ec302bf2a0ad592422099
vanshikabhati868-cmd <vanshikabhati868@gmail.com>
2026-03-09 13:17:32 +0530

    Delete index.js

--- pages/api/index.js
+++
@@ -1,8 +0,0 @@
-// Sample content of index.js
-// This file is generated from index.jd
-
-const handler = (req, res) => {
-    res.status(200).json({ message: 'Hello from API' });
-};
-
-export default handler;
